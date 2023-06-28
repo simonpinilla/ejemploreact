129e46420996ad9d051b9ff4bd41b70778ab8bba
simonpinilla <121509414+simonpinilla@users.noreply.github.com>
2023-06-28 19:11:35 +0000

    uso del Todos

--- ejemploreact/src/componentes/TodoList.jsx
+++ ejemploreact/src/componentes/TodoList.jsx
@@ -1,13 +1,57 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useEffect, useRef, useState } from "react"
 import { TodoItem } from "./TodoItem"
+
+const KEY="Todo-list-todos";
+
+export function TodoList(){
+    const [todos,sertodos,]=useState(
+
+
+        JSON.parse(localStorage.getItem(KEY))?JSON.parse(localStorage.getItem(KEY)):[]
+    )
+}
+
+const taskRef=useRef();
+
+useEffect(()=>{
+    localStorage.setItem(KEY,JSON.stringify(todos));
+    },[todos]);
+
+const agregarTarea=()=>{
+    const task=taskRef.current.value;
+    const id=uuid();
+    if(task=='')return;
+    setTodos((prevTodos)=>{
+        const newTask = {
+            id:id,
+            task:task,
+            complete:false
+        }
+        return[...prevTodos,newTask]
+
+    });
+    taskRef.current.value="";
+
+
+
+
+}
+
 
 export function TodoList(){
     return (
         <>
+            
             <h1>Lista de Tareas</h1>
-            <ul class="list-group">
-                <TodoItem></TodoItem>
-            </ul>
+            <div className="input-group mb-3 at-4">
+                <input ref={taskRef} placeholder="Ingrese una Tarea" className="form-control" type="text" id="">   
+                </input>
+                <button onClick={agregarTarea} className="btn-success ms-2"><i className="bi bi-plus-circle-fill"></i></button>
+                <button onClick={eliminarTareasCompletadas} className="btn-btn-danger ms-2"><i className="bi bi-trash"></i></button>
+                
+                
+            </div>
+            
         </>
     )
 
